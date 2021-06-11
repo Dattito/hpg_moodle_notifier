4f78d3e04d340c4e079ba82e7c5eb050fa9f0db2
Dattito <d.siregar@web.de>
2021-06-11 13:56:13 +0200

    step one

--- substituationplan_updater/go.mod
+++ substituationplan_updater/go.mod
@@ -1,3 +1,5 @@
 module github.com/Dattito/HSN_updater
 
 go 1.16
+
+require github.com/PuerkitoBio/goquery v1.6.1 // indirect
--- substituationplan_updater/main.go
+++ substituationplan_updater/main.go
@@ -1,5 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 
+	"github.com/PuerkitoBio/goquery"
+)
+
+func ExampleScrape() {
+	// Request the HTML page.
+
+	authid := "20052003DavSir"
+	authpw := "a0a179"
+
+	res, err := http.PostForm(fmt.Sprintf("https://vertretungsplan.hpg-speyer.de/pmwiki/pmwiki.php?n=Main.%s", authid),
+		url.Values{
+			"authid": {authid},
+			"authpw": {authpw},
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find the review items
+	s := doc.Find("table").First()
+
+	fmt.Println(s.Html())
+}
+
+func main() {
+	ExampleScrape()
 }
